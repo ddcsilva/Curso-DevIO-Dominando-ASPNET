e67217f3586dc7343ddd7af910580ea1f22b0234
Danilo Silva <danilo.silva@msn.com>
2023-01-26 20:39:17 -0300

    Criando um projeto MVC sem template

---
+++ AppModelo/src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+    }
+}

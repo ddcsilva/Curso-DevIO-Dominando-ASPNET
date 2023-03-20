52c65374a882fa083142774bc0fcf6cd6b40f4ab
danilo.silva <danilo.silva@msn.com>
2023-03-20 18:05:36 -0300

    6.0 - Mapeando as entidades para o banco de dados

--- Aplicacao-Completa/6.0/AppMvcCompleta/src/DevIO.App/Program.cs
+++ Aplicacao-Completa/6.0/AppMvcCompleta/src/DevIO.App/Program.cs
@@ -1,22 +1,32 @@
 using DevIO.App.Data;
+using DevIO.Data.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Guardando a connection string do arquivo appSettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+
+// Adicionando o suporte ao acesso ao DB do Identity via EF
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+// Adicionando o suporte ao acesso ao DB via EF
+builder.Services.AddDbContext<MeuDbContext>(options => options.UseSqlServer(connectionString));
+
+// Adicionando a tela de erro de banco de dados (para desenvolvimento)
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+// Adicionando o Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Adicionando o MVC
 builder.Services.AddRazorPages();
 
+// Gerando a APP
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração conforme os ambientes
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,7 +34,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -36,6 +45,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
---
+++ Aplicacao-Completa/6.0/AppMvcCompleta/src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,31 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context;
+
+public class MeuDbContext : DbContext
+{
+    public MeuDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Fornecedor> Fornecedores { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+        {
+            property.SetColumnType("varchar(100)");
+        }
+
+        // Registrar todos os mappings via Reflection
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+        }
+
+        base.OnModelCreating(modelBuilder);
+    }
+}

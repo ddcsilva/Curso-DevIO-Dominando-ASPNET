a20668badb887a9e61f86b0da6fdbc5de0a39bee
danilo.silva <danilo.silva@msn.com>
2023-03-23 14:34:12 -0300

    6.0 - Organizando e otimizando a estrutura da App

--- Aplicacao-Completa/6.0/AppMvcCompleta/src/DevIO.App/Program.cs
+++ Aplicacao-Completa/6.0/AppMvcCompleta/src/DevIO.App/Program.cs
@@ -1,55 +1,32 @@
-using DevIO.App.Data;
-using DevIO.Business.Interfaces;
+using DevIO.App.Configurations;
 using DevIO.Data.Context;
-using DevIO.Data.Repository;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
 
 // Guardando a connection string do arquivo appSettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Adicionando o suporte ao acesso ao DB do Identity via EF
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+// Adicionando o Identity
+builder.Services.AddIdentityConfiguration(builder.Configuration);
 
 // Adicionando o suporte ao acesso ao DB via EF
 builder.Services.AddDbContext<MeuDbContext>(options => options.UseSqlServer(connectionString));
-
-// Adicionando a tela de erro de banco de dados (para desenvolvimento)
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-// Adicionando o Identity
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Adicionando o AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
 
 // Adicionando o MVC
-builder.Services.AddControllersWithViews(o =>
-{
-    o.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O valor preenchido é inválido para este campo.");
-    o.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor(x => "Este campo precisa ser preenchido.");
-    o.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(() => "Este campo precisa ser preenchido.");
-    o.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que o body na requisição não esteja vazio.");
-    o.ModelBindingMessageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
-    o.ModelBindingMessageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O valor preenchido é inválido para este campo.");
-    o.ModelBindingMessageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser numérico");
-    o.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
-    o.ModelBindingMessageProvider.SetValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
-    o.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(x => "O campo deve ser numérico.");
-    o.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(x => "Este campo precisa ser preenchido.");
+builder.Services.AddMvcConfiguration();
 
-    o.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-});
-
-builder.Services.AddScoped<MeuDbContext>();
-builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
-builder.Services.AddScoped<IFornecedorRepository, FornecedorRepository>();
-builder.Services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+// Resolvendo dependências
+builder.Services.ResolveDependencies();
 
 // Gerando a APP
 var app = builder.Build();
@@ -57,11 +34,13 @@ var app = builder.Build();
 // Configuração conforme os ambientes
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/erro/500");
+    app.UseStatusCodePagesWithRedirects("/erro/{0}");
     app.UseHsts();
 }
 
@@ -73,14 +52,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-var defaultCulture = new CultureInfo("pt-BR");
-var localizationOptions = new RequestLocalizationOptions
-{
-    DefaultRequestCulture = new RequestCulture(defaultCulture),
-    SupportedCultures = new List<CultureInfo> { defaultCulture },
-    SupportedUICultures = new List<CultureInfo> { defaultCulture }
-};
-app.UseRequestLocalization(localizationOptions);
+app.UseGlobalizationConfig();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
